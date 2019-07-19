201dce6de985f7a98e5e338199d252c3acf60174
“Dean” <467271883@qq.com>
2019-07-19 15:43:47 +0800

    Initial commit

---
+++ src/components/Counter.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <button @click="add">+</button>
+    <span>{{value}}</span>
+    <button @click="del">-</button>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "Counter",
+    data () {
+      return {
+        value: 0
+      }
+    },
+    methods: {
+      add(){
+        this.value++;
+      },
+      del(){
+        this.value--;
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
